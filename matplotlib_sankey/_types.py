1cf20aa2b2833047c880a9ab441a79ea43f1b674
Malte <harryhaller001@gmail.com>
2025-04-26 21:49:13 +0200

    update package boilerplate

--- matplotlib_sankey/_types.py
+++ matplotlib_sankey/_types.py
@@ -1,8 +1,8 @@
 from collections.abc import Sequence
-from typing import Literal, Tuple, TypeAlias, Union
+from typing import Literal, TypeAlias
 
 from matplotlib.colors import Colormap
 
 CurveType: TypeAlias = Literal["curve3", "curve4", "line"]
 
-AcceptedColors: TypeAlias = Union[Sequence[str], Colormap, str, Sequence[Tuple[float, float, float]]]
+AcceptedColors: TypeAlias = Sequence[str] | Colormap | str | Sequence[tuple[float, float, float]]
--- tests/test_sankey_plot.py
+++ tests/test_sankey_plot.py
@@ -1,9 +1,11 @@
-import numpy as np
-
-from matplotlib_sankey import sankey_simple
+from matplotlib_sankey import sankey
 
 
 def test_sankey_simple_plot():
     """Testing simple sankey plot."""
-    data = np.array([[10, 24, 50, 80], [14, 30, 24, 40], [4, 10, 50, 53]])
-    sankey_simple(data)
+    data = [
+        [(0, 2, 20), (0, 1, 10), (3, 4, 15), (3, 2, 10), (5, 1, 5), (5, 2, 50)],
+        [(2, 6, 40), (1, 6, 15), (2, 7, 40), (4, 6, 15)],
+        [(7, 8, 5), (7, 9, 5), (7, 10, 20), (7, 11, 10), (6, 11, 55), (6, 8, 15)],
+    ]
+    sankey(data)
